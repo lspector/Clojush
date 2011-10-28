a1bc91a1a7ea794271bc0eaa3e386373f4b33fe2
Tom Helmuth <trhtom@gmail.com>
2011-10-28 15:37:46 -0400

    Got simple pushgp string example working.

--- src/examples/string.clj
+++ src/examples/string.clj
@@ -5,12 +5,21 @@
 ; Tries to get a string with the most unique characters. If there are at least 8, succeeds.
 (pushgp :error-function (fn [program]
                           (list (let [final-state (run-push program (make-push-state))
-                                top-string (top-item :string final-state)]
-                            (if (not (string? top-string))
-                              1000
-                              (let [unique-chars (count (distinct top-string))]
-                                (if (> unique-chars 8)
-                                  0
-                                  (- 8 unique-chars)))))))
-        	 :atom-generators (list 'string_rand)
-        	 :tournament-size 5)
+                                      top-string (top-item :string final-state)]
+                                  (if (not (string? top-string))
+                                    1000
+                                    (let [unique-chars (count (distinct top-string))]
+                                      (if (>= unique-chars 8)
+                                        0
+                                        (- 8 unique-chars)))))))
+        	 :atom-generators (list (fn [] (apply str (repeatedly (+ 1 (lrand-int 8))
+                                                               #(rand-nth (str "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                                                                               "abcdefghijklmnopqrstuvwxyz"
+                                                                               "1234567890"))))))
+                                   ;'string_rand)
+                                   	 :tournament-size 5)
+                                 
+
+
+; Add string_concat to this example, and make the number of unique characters higher. Maybe only use
+; upper case, and try to evolve a string with all 26 letters.
