daba2075edcaefb22d9de20d0fef4075db11fd9f
Tom Helmuth <trhtom@gmail.com>
2012-06-21 13:26:02 -0400

    Moved boolean instructions to instructions/boolean.clj

--- src/clojush/clojush.clj
+++ src/clojush/clojush.clj
@@ -88,55 +88,6 @@
       :string
       state)))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; instructions for Booleans
-
-(define-registered 
-  boolean_and
-  (fn [state]
-    (if (not (empty? (rest (:boolean state))))
-      (push-item (and (stack-ref :boolean 0 state)
-                      (stack-ref :boolean 1 state))
-                 :boolean
-                 (pop-item :boolean (pop-item :boolean state)))
-      state)))
-
-(define-registered 
-  boolean_or
-  (fn [state]
-    (if (not (empty? (rest (:boolean state))))
-      (push-item (or (stack-ref :boolean 0 state)
-                     (stack-ref :boolean 1 state))
-                 :boolean
-                 (pop-item :boolean (pop-item :boolean state)))
-      state)))
-
-(define-registered boolean_not
-                   (fn 
-                     [state]
-                     (if (not (empty? (:boolean state)))
-                       (push-item (not (stack-ref :boolean 0 state))
-                                  :boolean
-                                  (pop-item :boolean state))
-                       state)))
-
-(define-registered 
-  boolean_frominteger
-  (fn [state]
-    (if (not (empty? (:integer state)))
-      (push-item (not (zero? (stack-ref :integer 0 state)))
-                 :boolean
-                 (pop-item :integer state))
-      state)))
-
-(define-registered 
-  boolean_fromfloat
-  (fn [state]
-    (if (not (empty? (:float state)))
-      (push-item (not (zero? (stack-ref :float 0 state)))
-                 :boolean
-                 (pop-item :float state))
-      state)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; instructions for strings
