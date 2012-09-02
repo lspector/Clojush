853ad6cb50854e19c9ac656151ca1ffa9370ead0
Lee Spector <lspector@hampshire.edu>
2012-09-02 04:38:34 -0400

    Minor adjustments to correct glitches in git/clojure update sequence.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lspector/clojush "1.2.8"
+(defproject lspector/clojush "1.2.9"
             :description "The Push programming language and the PushGP genetic programming
                           system implemented in Clojure. 
                           See http://hampshire.edu/lspector/push.html"
