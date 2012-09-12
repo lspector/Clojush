ee01279a6b184d09117af5fcd4df7bb3eeae52dc
Tom Helmuth <thelmuth@cs.umass.edu>
2012-09-12 11:45:36 -0400

    Started working on bioavailability problem.

---
+++ src/clojush/examples/bioavailability.clj
@@ -0,0 +1,19 @@
+;; bioavailability.clj
+;;
+;; Sara Silva and Leonardo Vanneschi. 2009. Operator equalisation,
+;; bloat and overfitting: a study on human oral bioavailability
+;; prediction. In Proceedings of the 11th Annual conference on
+;; Genetic and evolutionary computation (GECCO '09). ACM,
+;; New York, NY, USA, 1115-1122. DOI=10.1145/1569901.1570051
+;; http://doi.acm.org/10.1145/1569901.1570051 
+;;
+;; Data available from:
+;;  http://personal.disco.unimib.it/Vanneschi/bioavailability.txt
+;;
+;; Tom Helmuth, thelmuth@cs.umass.edu, 2012
+
+(ns clojush.examples.bioavailability
+  (:use [clojush.pushgp.pushgp]
+        [clojush.pushstate]
+        [clojush.interpreter]
+        [local-file]))
