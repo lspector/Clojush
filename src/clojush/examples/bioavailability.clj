9692cd5aa25169c0afff2767f79180bd9738c185
Tom Helmuth <thelmuth@cs.umass.edu>
2012-09-18 14:28:08 -0400

    Added information about the BA problem and some more functionality for training and test sets.

--- src/clojush/examples/bioavailability.clj
+++ src/clojush/examples/bioavailability.clj
@@ -1,5 +1,20 @@
 ;; bioavailability.clj
 ;;
+;; This problem is a symbolic regression problem with 359 test cases
+;; over 241 variables. Each test case is a candidate drug compound,
+;; with each variable being a bi-dimensional molecule descriptor of
+;; that compound. The data file data/bioavailability.txt
+;; contains the data, with the last column being the target variable,
+;; in this case human oral bioavailability (%F), a float in the
+;; range [0.0, 100.0].
+;;
+;; The experimental procedure is copied from the paper below. This
+;; procedure uses 70% of the test cases as a training set, and the
+;; remaining 30% as a test set. Each time this file is run, it will
+;; select random training (251 test cases) and test (108 test cases)
+;; sets from the test cases to use throughout the entire run.
+;;
+;; See this paper for more information about this problem:
 ;; Sara Silva and Leonardo Vanneschi. 2009. Operator equalisation,
 ;; bloat and overfitting: a study on human oral bioavailability
 ;; prediction. In Proceedings of the 11th Annual conference on
@@ -27,6 +42,27 @@
                %)
          lines)))
 
-(count (read-data))
+;(count (read-data))
 
-(map last (read-data))
+;(count (first (read-data)))
+
+;(map last (read-data))
+
+;(def test-cases-shuffled
+;  (shuffle (read-data)))
+
+
+(defn define-test-cases
+  []
+  (let [test-cases-shuffled (shuffle (read-data))
+        train-num 251]
+    {:train (subvec test-cases-shuffled 0 train-num)
+     :test (subvec test-cases-shuffled train-num)}))
+
+
+(count (:test (define-test-cases)))
+
+
+
+
+
