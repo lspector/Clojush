f9494bc0c912d26ab099cdb8a5f53d255725a14e
Lee Spector <lspector@hampshire.edu>
2012-09-25 22:02:44 -0400

    RMSE-error; bioavailability problem.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lspector/clojush "1.2.13"
+(defproject lspector/clojush "1.2.14"
             :description "The Push programming language and the PushGP genetic programming
                           system implemented in Clojure. 
                           See http://hampshire.edu/lspector/push.html"
