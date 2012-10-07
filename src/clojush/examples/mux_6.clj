7e59ae2d2fe9e8d65b31cfa48000eccdd052ecb9
Lee Spector <lspector@hampshire.edu>
2012-10-07 07:31:43 -0400

    Mux6 and valiant with lexicase and geometric semantic xover hacks.

--- src/clojush/examples/mux_6.clj
+++ src/clojush/examples/mux_6.clj
@@ -10,6 +10,47 @@
         [clojush.interpreter]
         [clojure.math.numeric-tower]))
 
+(in-ns 'clojush.pushgp.genetic-operators)
+
+#_(defn mutate 
+  "Returns a mutated version of the given individual."
+  [ind mutation-max-points max-points atom-generators]
+  (let [new-program (case (lrand-int 5)
+                      0 (insert-code-at-point (:program ind) 
+                                              (select-node-index (:program ind))
+                                              (random-code mutation-max-points atom-generators))
+                      1 (list 'if-dirty (:program ind) (random-code mutation-max-points atom-generators))
+                      2 (list 'if-dirty (random-code mutation-max-points atom-generators) (:program ind))
+                      3 (list 'if-obstacle (:program ind) (random-code mutation-max-points atom-generators))
+                      4 (list 'if-obstacle (random-code mutation-max-points atom-generators) (:program ind)))]
+    (if (> (count-points new-program) max-points)
+      ind
+      (make-individual :program new-program :history (:history ind)
+                       :ancestors (if maintain-ancestors
+                                    (cons (:program ind) (:ancestors ind))
+                                    (:ancestors ind))))))
+
+(defn crossover 
+  "Returns a copy of parent1 with a random subprogram replaced with a random 
+   subprogram of parent2."
+  [parent1 parent2 max-points]
+  (let [new-program (case (lrand-int 2)
+                      0 (insert-code-at-point 
+                          (:program parent1) 
+                          (select-node-index (:program parent1))
+                          (code-at-point (:program parent2)
+                                         (select-node-index (:program parent2))))
+                      1 (list (random-code 10 @global-atom-generators) 'exec_if (:program parent1) (:program parent2)))]
+    (if (> (count-points new-program) max-points)
+      parent1
+      (make-individual :program new-program :history (:history parent1)
+                       :ancestors (if maintain-ancestors
+                                    (cons (:program parent1) (:ancestors parent1))
+                                    (:ancestors parent1))))))
+
+(in-ns 'clojush.examples.mux-6)
+
+
 ;; We store address bits in a vector on top of the auxiliary stack
 ;; and data bits in a vector under the address bits vector.
 
@@ -74,4 +115,9 @@
                                ;boolean_dup boolean_swap boolean_pop boolean_rot
                                )
   :max-points 200
-  :max-points-in-initial-program 200)
+  :max-points-in-initial-program 20
+  :use-lexicase-selection true
+  :mutation-probability 0.1
+  :crossover-probability 0.5
+  :simplification-probability 0.4
+  )
