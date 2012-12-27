9b90e35746e2f354c1be796d7cd2234da64b3c7a
Lee Spector <lspector@hampshire.edu>
2012-12-27 17:42:39 -0500

    Use records for pushstates.

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject lspector/clojush "1.2.32"
+(defproject lspector/clojush "1.2.33"
             :description "The Push programming language and the PushGP genetic programming
-                          system implemented in Clojure. 
+                          system implemented in Clojure.
                           See http://hampshire.edu/lspector/push.html"
             :dependencies [[org.clojure/clojure "1.3.0"]
-                           [org.clojure/math.numeric-tower "0.0.1"]   
+                           [org.clojure/math.numeric-tower "0.0.1"]
                            [local-file "0.0.4"]
                            [clojure-csv "2.0.0-alpha2"]
                            [org.clojure/data.json "0.1.3"]]
