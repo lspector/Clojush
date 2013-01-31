4a93cc7afceac86b3367f68a8a46ac94b1b66ff9
Emma Tosch <etosch@gmail.com>
2013-01-31 17:26:35 -0500

    added new anaylsis file with Log defrecord

---
+++ src/analysis/core.clj
@@ -0,0 +1,63 @@
+(ns analysis.core
+  (:require [clojure.java.io :as io])
+
+;; don't assume that we can hold the whole file in memory
+
+;; registered instructions
+;; push parameters
+;; generation reports
+;; summary
+
+;; need a collection report structured object
+;; get the error vectors from the first generation
+
+(defrecord Log [insts params reports summary])
+
+(defn make-inst [inst-chunk]
+  (-> inst-chunk
+      (s/split #":")
+      (second)
+      (read-string)))
+
+(defn make-params [param-chunk]
+  (->> param-chunk
+       (map #(s/split % #"="))
+       (map #(vector (s/trim (% 0)) (s/trim (% 1))))
+       (reduce #(assoc %1 (%2 0) (%2 1)) (sorted-map))))
+
+(defn make-reports [reports-chunk]
+  (let [pred #(.contains % ";;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;")]   
+    (for [chunk (remove pred (partition-by pred reports-chunk))]
+      (->> chunk
+           (map #(s/split % #":"))
+           (remove #(not= 2 (count %)))
+           (flatten)
+           (map s/trim)
+           (apply sorted-map)))))
+
+(defn make-summary [summary-chunk]
+  (->> summary-chunk
+       (map #(s/split % #":"))
+       (filter #(= 2 (count %)))
+       (flatten)
+       (map s/trim)
+       (apply sorted-map)))
+      
+
+(defn parse-Log [log-file]
+  (let [lines (s/split-lines (slurp log-file))
+        inst-chunk (first (filter #(.contains % "Registered instructions:") lines))
+        param-chunk (filter #(.contains % "=") lines)
+        reports-chunk (->> lines
+                           (drop-while #(not (.contains % ";;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;")))
+                           (take-while #(and (not (.contains % "SUCCESS")) (not (.contains % "FAILURE")))))
+        summary-chunk (drop-while #(and (not (.contains % "SUCCESS")) (not (.contains % "FAILURE"))) lines)]
+    (Log. (make-inst inst-chunk)
+          (make-params param-chunk)
+          (make-reports reports-chunk)
+          (make-summary summary-chunk))))
+
+  
+(defn aggregate-logs [log-folder]
+  (for [log (.listFiles (io/file log-folder))]
+    (parse-Log log)))
