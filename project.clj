f15e72b5c29bde3b188d45cb9d8bd7c81bc96a63
Lee Spector <lspector@hampshire.edu>
2013-03-15 09:09:27 -0400

    Version bump.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lspector/clojush "1.3.7"
+(defproject lspector/clojush "1.3.8"
             :description "The Push programming language and the PushGP genetic programming
                           system implemented in Clojure.
                           See http://hampshire.edu/lspector/push.html"
