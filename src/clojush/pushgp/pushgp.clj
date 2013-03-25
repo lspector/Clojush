e1836c78ba8d4cf877c0023249fba07f89474107
Tom Helmuth <thelmuth@cs.umass.edu>
2013-03-25 19:19:58 -0400

    Adds stack trace printing on exceptions.

--- src/clojush/pushgp/pushgp.clj
+++ src/clojush/pushgp/pushgp.clj
@@ -83,7 +83,7 @@
 
 (defn make-agents-and-rng [{:keys [initial-population use-single-thread population-size
                                    max-points-in-initial-program atom-generators random-seed]}]
-  (let [agent-error-handler (fn [agnt except] (println except) (System/exit 0))]
+  (let [agent-error-handler (fn [agnt except] (.printStackTrace except) (System/exit 0))]
     {:pop-agents (if initial-population
                    (->> (read-string (slurp (str "data/" initial-population)))
                         (map #(if use-single-thread (atom %) (agent %)))
