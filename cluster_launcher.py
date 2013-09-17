bcaf845fa064d45337b5aa2a09931ea18e0a4d7c
Tom Helmuth <thelmuth@cs.umass.edu>
2013-09-17 11:40:22 -0400

    Updated cluster_launcher.py for better functionality.

--- cluster_launcher.py
+++ cluster_launcher.py
@@ -1,18 +1,31 @@
 import os, stat
 
+##########################################################################
 # Settings
 number_runs = 10
 
 clojush_directory = "/home/thelmuth/Clojush/"
 output_directory = "../Results/odd/"
-output_prefix = "log"
-output_postfix = ".txt"
+
+example_file = "clojush.examples.odd"
 
 title_string = "Test of cluster runs with odd problem"
 
-example_file = "clojush.examples.odd"
+description = """This description will be stored in a file alongside the logs from the runs. This is just a test using the odd problem.
+"""
+
+##########################################################################
+# Uncomment the following if you want to print timings in the logs
+example_file += " :print-timings true"
+
+##########################################################################
+# Probably don't change these
+output_prefix = "log"
+output_postfix = ".txt"
+
 command = "/share/apps/bin/lein with-profiles production trampoline run " + example_file
 
+service_tag = "tom"
 
 ##########################################################################
 # You don't need to change anything below here
@@ -22,6 +35,13 @@ if os.path.isdir(clojush_directory + output_directory):
     raise RuntimeError, "Output directory already exists"
 
 os.mkdir(clojush_directory + output_directory)
+
+# Make description file
+description_file_string = output_directory + "description.txt"
+description_f = open(description_file_string, "w")
+
+description_f.writelines(title_string + "\n\n" + description)
+description_f.close()
 
 # Make alf file
 alf_file_string = output_directory + "clojush_runs.alf"
@@ -38,9 +58,9 @@ for run in range(0, number_runs):
     full_command = intro_command + command + outro_command
 
     alfcode += """    Task -title {%s - run %i} -cmds {
-        RemoteCmd {/bin/sh -c "%s"} -service {cluster}
+        RemoteCmd {/bin/sh -c "%s"} -service {%s}
     }
-""" % (title_string, run, full_command)
+""" % (title_string, run, full_command, service_tag)
 
 alfcode += "}\n"
 
