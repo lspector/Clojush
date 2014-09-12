5288befa51ae779a502e9b339f823aeca396e399
Tom Helmuth <thelmuth@cs.umass.edu>
2014-09-12 08:57:18 -0400

    Updated clj-random version

--- project.clj
+++ project.clj
@@ -10,7 +10,7 @@
                  [local-file "0.1.0"]
                  [clojure-csv "2.0.0-alpha2"]
                  [org.clojure/data.json "0.1.3"]
-                 [clj-random "0.1.5"]
+                 [clj-random "0.1.7"]
                  ;[incanter/incanter-core "1.5.2"]
                  ]
   :dev-dependencies [[lein-ccw "1.2.0"]]
