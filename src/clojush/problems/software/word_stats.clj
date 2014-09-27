a1f9df68e0d600eda30d829ca8a46ad6f041ca0c
Tom Helmuth <thelmuth@cs.umass.edu>
2014-09-26 22:19:42 -0400

    Changed printing of test cases to true for Word Stats

--- src/clojush/problems/software/word_stats.clj
+++ src/clojush/problems/software/word_stats.clj
@@ -190,7 +190,7 @@
   (let [[train-cases test-cases] (map #(sort-by (comp count first) %)
                                       (map word-stats-test-cases
                                            (test-and-train-data-from-domains data-domains)))]
-    (when false ;; Change to false to not print test cases
+    (when true ;; Change to false to not print test cases
       (doseq [[i case] (map vector (range) train-cases)]
         (println (format "Train Case: %3d | Input/Output: %s" i (str case))))
       (doseq [[i case] (map vector (range) test-cases)]
