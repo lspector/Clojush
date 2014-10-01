2bea2a59f2d7941a8e0acbbb758162cdfdc728bb
Lee Spector <lspector@hampshire.edu>
2014-10-01 09:27:30 -0400

    thelmuth/vector-new-instructions

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojush "2.0.6"
+(defproject clojush "2.0.7"
   :description "The Push programming language and the PushGP genetic programming
                 system implemented in Clojure.
                 See http://hampshire.edu/lspector/push.html"
