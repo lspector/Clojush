6318f0875983dbd30c7bbc28428d23105b1b205d
Lee Spector <lspector@hampshire.edu>
2015-02-22 20:38:09 -0500

    thelmuth/restore-number-io

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojush "2.0.14"
+(defproject clojush "2.0.15"
   :description "The Push programming language and the PushGP genetic programming
                 system implemented in Clojure.
                 See http://hampshire.edu/lspector/push.html"
