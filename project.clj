7dad9b8e334624e70007df2f7c551e4dd3ccc15b
Lee Spector <lspector@hampshire.edu>
2015-03-08 10:36:31 -0400

    Vaguery/code-literals

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojush "2.0.20"
+(defproject clojush "2.0.21"
   :description "The Push programming language and the PushGP genetic programming
                 system implemented in Clojure.
                 See http://hampshire.edu/lspector/push.html"
