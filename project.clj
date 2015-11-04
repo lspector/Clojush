5ea1e373f3e5c2d871b3dba85f835ce44dd4483a
deploy.sh <_@_>
2015-11-04 09:43:56 -0500

    Version 2.0.49-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojush2 "2.0.48"
+(defproject clojush2 "2.0.49-SNAPSHOT"
   :description "The Push programming language and the PushGP genetic programming
                 system implemented in Clojure.
                 See http://hampshire.edu/lspector/push.html"
