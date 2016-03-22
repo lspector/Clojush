5233a92044dda465da75ece2c4b74240fb32b484
Lee Spector <lspector@hampshire.edu>
2016-03-21 22:07:16 -0400

    Added clojush.problems.software.levenshtein.

---
+++ src/clojush/problems/software/levenshtein.clj
@@ -0,0 +1,87 @@
+(ns clojush.problems.software.levenshtein
+  (:use clojush.pushgp.pushgp
+        [clojush pushstate interpreter random util globals]
+        clojush.instructions.tag
+        clojure.math.numeric-tower)
+  (:require [clojure.string :as string]))
+
+(defn random-string
+  "Returns a random string of letters, digits, and spaces, of max length n."
+  [n]
+  (apply str 
+         (repeatedly (lrand-int (inc n)) 
+                     (fn []
+                       (if (< (lrand) 0.2)
+                         \space
+                         (lrand-nth (map char (range 32 127))))))))
+
+(def levenshtein-atom-generators
+  (concat (list
+            (fn [] (- 100 (lrand-int 201)))
+            (tag-instruction-erc [:exec :integer :boolean :string :char] 1000)
+            (tagged-instruction-erc 1000)
+            'in1
+            'in2)
+          (registered-for-stacks [:integer :boolean :string :char :exec])))
+
+(defn generate-levenshtein-cases
+  []
+  (repeatedly 5 (fn []
+                  (let [str1 (random-string 100)
+                        str2 (random-string 100)]
+                    [[str1 str2] (levenshtein-distance str1 str2)]))))
+
+(def levenshtein-cases (atom (generate-levenshtein-cases)))
+
+(println "Initial test cases:" (pr-str @levenshtein-cases))
+
+(defn levenshtein-error
+  [program]
+  (let [behavior (atom '())
+        errors (doall
+                 (for [[[str1 str2] distance] @levenshtein-cases]
+                   (let [state (run-push program 
+                                         (push-item str2 :input 
+                                                    (push-item str1 :input 
+                                                               (make-push-state))))
+                         top-int (top-item :integer state)]
+                     (when @global-print-behavioral-diversity
+                       (swap! behavior conj [top-int]))
+                     (if (number? top-int)
+                       (#(if (neg? %) (- %) %) (- top-int distance))
+                       1000))))]
+    (when @global-print-behavioral-diversity
+      (swap! population-behaviors conj @behavior))
+    errors))
+
+(defn levenshtein-report
+  "Not actually used to report; instead, change test cases."
+  [best population generation error-function report-simplifications]
+  (let [new-cases (generate-levenshtein-cases)]
+    (println "New test cases:" (pr-str new-cases))
+    (reset! levenshtein-cases new-cases)))
+
+(def argmap
+  {:error-function levenshtein-error
+   :atom-generators levenshtein-atom-generators
+   :max-points 1600
+   :max-genome-size-in-initial-program 400
+   :evalpush-limit 1600
+   :population-size 1000
+   :max-generations 300
+   :parent-selection :lexicase
+   :genetic-operator-probabilities {:alternation 0.2
+                                    :uniform-mutation 0.2
+                                    :uniform-close-mutation 0.1
+                                    [:alternation :uniform-mutation] 0.5
+                                    }
+   :alternation-rate 0.01
+   :alignment-deviation 10
+   :uniform-mutation-rate 0.01
+   :problem-specific-report levenshtein-report
+   :print-behavioral-diversity true
+   :report-simplifications 0
+   :final-report-simplifications 5000
+   :max-error 5000
+   :use-single-thread false
+   })
