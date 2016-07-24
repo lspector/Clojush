1e0a01c3d415f0901abef14ff2a02e0f3a585a01
Tom Helmuth <helmutht@wlu.edu>
2016-07-24 07:41:51 -0600

    Working on Plush genome autosimplification

--- src/clojush/simplification.clj
+++ src/clojush/simplification.clj
@@ -1,5 +1,5 @@
 (ns clojush.simplification
-  (:use [clojush util globals pushstate random individual evaluate]))
+  (:use [clojush util globals pushstate random individual evaluate translate]))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; auto-simplification
@@ -91,4 +91,122 @@
                    steps
                    print?
                    progress-interval)))
-                                  
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; auto-simplification of Plush genomes
+
+(defn select-random-weighted-item
+  [prob-map]
+  (let [summed-probs (reductions (fn [[_ prev-prob] [item prob]]
+                                   (vector item (+ prev-prob prob)))
+                                 prob-map)
+        rand-num (rand (second (last summed-probs)))]
+    (loop [items-and-probs summed-probs]
+      (if (< rand-num (second (first items-and-probs)))
+        (first (first items-and-probs))
+        (recur (rest items-and-probs))))))
+
+(defn apply-simplification-step-to-genome
+  [genome simplification-step-probabilities]
+  (let [transformation-map (select-random-weighted-item simplification-step-probabilities)]
+    ;;;MAKE CHANGES HERE
+
+(defn auto-simplify-plush
+  "Automatically simplifies the genome of an individual without changing its error vector on
+   the training set, based on the error-function. steps is the number of hill-climbing evaluations
+   to test. print-progress-interval is how often to print progress of the simplification; if it is
+   set to 0, then nothing will be printed.
+   simplification-step-probabilities is a map of probabilities that are used to select what change
+   to make during each step of the simplification. Each change is represented as a map with the
+   following options for the keys, each of which has an integer of how many of those changes to make:
+     :silence - number of unsilenced or no-op genes to set :silent = true
+     :unsilence - number of silenced or no-op genes to set :silent = false
+     :no-op - number of unsilenced or silenced genes to set :silent = :no-op
+"
+  ([ind error-function steps print-progress-interval]
+    (auto-simplify-plush ind error-function steps print-progress-interval
+                         {{:silence 1} 0.5
+                          {:silence 2} 0.3
+                          {:silence 3} 0.1
+                          {:silence 4} 0.1
+                          ;{:silence 1 :unsilence 1} 0.05 ;Not used by default
+                          ;{:silence 2 :unsilence 1} 0.1 ;Not used by default
+                          ;{:silence 3 :unsilence 1} 0.05 ;Not used by default
+                          ;{:no-op 1} 0.1 ;Not used by default
+                          }))
+  ([ind error-function steps print-progress-interval simplification-step-probabilities]
+    (when (not (zero? print-progress-interval))
+      (printf "\nAuto-simplifying Plush genome with starting size: %s" (count-points (:program ind))))
+    (loop [step 0
+           genome (:genome ind)
+           program (if (:program ind)
+                     (:program ind)
+                     (translate-plush-genome-to-push-program ind
+                                                             {:max-points (* 10 (count genome))}))
+           errors (if (:errors ind)
+                    (:errors ind)
+                    (error-function program))]
+;           total-errors (if (:total-error ind)
+;                          (:total-error ind)
+;                          (apply + errors))]
+      (when (and (not (zero? print-progress-interval))
+                 (or (>= step steps)
+                     (zero? (mod step print-progress-interval))))
+        (println "\nstep:" step)
+        (println "genome:" (pr-str (not-lazy genome)))
+        (println "program:" (pr-str (not-lazy program)))
+        (println "errors:" (not-lazy errors))
+        (println "genome size:" (count genome))
+        (println "program size:" (count-points program)))
+      (if (>= step steps)
+        (make-individual :genome genome :program program :errors errors :total-error (apply + errors)
+                         :history (:history ind) :genetic-operators :simplification)
+        (let [new-genome (apply-simplification-step-to-genome genome simplification-step-probabilities)
+              new-program (translate-plush-genome-to-push-program ind
+                                                             {:max-points (* 10 (count genome))})
+              new-errors (error-function new-program)]
+          (if (= new-errors errors)
+            (recur (inc step) new-genome new-program new-errors)
+            (recur (inc step) genome program errors)))))))
+
+;;;;;;;;;;;;;;;
+(def ind-test
+  (make-individual :genome 
+                   '({:close 0, :instruction exec_shove}
+                      {:close 0, :instruction exec_eq}
+                      {:close 1, :instruction boolean_dup}
+                      {:close 0, :instruction boolean_eq}
+                      {:close 0,
+                       :instruction code_stackdepth}
+                      {:close 0,
+                       :instruction boolean_yankdup}
+                      {:close 0, :instruction 64}
+                      {:close 0, :instruction code_flush}
+                      {:close 1, :instruction code_yank}
+                      {:close 1, :instruction exec_yank}
+                      {:close 0, :instruction exec_swap}
+                      {:close 1, :instruction code_shove}
+                      {:close 0, :instruction integer_yank}
+                      {:close 0, :instruction exec_rot}
+                      {:close 0, :instruction code_flush}
+                      {:close 0, :instruction integer_swap}
+                      {:close 0, :instruction code_pop}
+                      {:close 0, :instruction code_pop}
+                      {:close 1, :instruction in1}
+                      {:close 1, :instruction code_yank})))
+;                   (random-plush-genome-with-size 20
+;                                                  (concat (registered-for-stacks [:integer :boolean :code :exec])
+;                                                          (list (fn [] (lrand-int 100))
+;                                                                'in1))
+;                                                  {:epigenetic-markers [:close]})))
+
+(require 'clojush.problems.demos.odd)
+
+(defn test-autosimp-genome
+  [ind]
+  (auto-simplify-plush ind
+                       (:error-function clojush.problems.demos.odd/argmap)
+                       1000
+                       100))
+
+(test-autosimp-genome ind-test)
