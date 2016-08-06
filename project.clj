638842e38685ea4e26b4c02e7840a3d8cda1bb8b
_ <_@_._>
2016-08-06 20:49:03 +0000

    Version 2.3.1 [ci skip]

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojush "2.3.1-SNAPSHOT"
+(defproject clojush "2.3.1"
   :description "The Push programming language and the PushGP genetic programming
                 system implemented in Clojure.
                 See http://hampshire.edu/lspector/push.html"
