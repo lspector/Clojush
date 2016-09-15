dc812d42811fade844d416d08e2cdfa9b9bf58a3
Lee Spector <lspector@hampshire.edu>
2016-09-15 17:38:07 -0400

    Moved push-argmap, arg documentation, and utility fns to new namespace clojush.args.

--- src/clojush/ns.clj
+++ src/clojush/ns.clj
@@ -3,6 +3,6 @@
 (defmacro use-clojush
   []
   '(do
-     (use '(clojush evaluate globals individual interpreter pushstate random simplification util))
+     (use '(clojush args evaluate globals individual interpreter pushstate random simplification util))
      (use '(clojush.instructions boolean code common numbers random-instructions return string tag zip))
      (use '(clojush.pushgp breed genetic-operators parent-selection pushgp report))))
