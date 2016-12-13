ce349e473a6a955daef9b6aab21d0c8634bd0274
Lee Spector <lspector@hampshire.edu>
2016-12-13 08:30:48 -0500

    Changed project.clj URL; lein release :patch

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
 (defproject clojush "2.15.1-1-SNAPSHOT"
   :description "The Push programming language and the PushGP genetic programming
                 system implemented in Clojure.
-                See http://hampshire.edu/lspector/push.html"
+                See http://pushlanguage.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
