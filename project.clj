8adbf3ded7086451aa9f6e2e1e147fc38eb1f055
Lee Spector <lspector@hampshire.edu>
2016-12-23 16:15:35 -0500

    lein release :patch

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject clojush "2.16.2-1-SNAPSHOT" 
   :description "The Push programming language and the PushGP genetic programming
                 system implemented in Clojure.
-                See http://pushlanguage.com"
+                See http://pushlanguage.com" 
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
