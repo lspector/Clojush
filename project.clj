d96c3cea0fdbf4759723265e2b108c6fd98217b4
_ <_@_._>
2017-03-04 01:09:25 +0000

    Version 2.20.0 [ci skip]

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojush "2.19.2-1-SNAPSHOT" 
+(defproject clojush "2.20.0" 
   :description "The Push programming language and the PushGP genetic programming
                 system implemented in Clojure. See http://pushlanguage.com" 
   :license {:name "Eclipse Public License"
