cd8272bbf58a6ffafbd8a7d0cf2a5b5e6296dbf6
Saul Shanabrook <s.shanabrook@gmail.com>
2017-07-08 23:42:23 -0400

    Move ultra to test profile

--- project.clj
+++ project.clj
@@ -20,8 +20,8 @@
             [lein-shell "0.5.0"]
             [lein-gorilla "0.4.0"]
             [cider/cider-nrepl "0.14.0"]
-            [lein-cloverage "1.0.6"]
-            [venantius/ultra "0.5.1"]]
+            [lein-cloverage "1.0.6"]]
+  :profiles {:text {:plugins [[venantius/ultra "0.5.1"]]}}
   :codox {:source-uri "http://github.com/lspector/Clojush/blob/master/{filepath}#L{line}"
           :namespaces [#"^(?!clojush\.problems)"]
           :output-path "doc"
