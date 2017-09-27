2fc3401f8abdc72b9574f23a3f58f575eec02530
Saul Shanabrook <s.shanabrook@gmail.com>
2017-09-27 11:39:39 -0400

    Generalize benchmarking code to allow getting any func

--- bench/clojush/interpreter_bench.clj
+++ bench/clojush/interpreter_bench.clj
@@ -7,42 +7,43 @@
             [clojush.core :refer [-main]]
             [clojush.interpreter :refer [eval-push]]))
 
-(defn grab-executions
-  "Returns the first `n` executions of the interpreter, from Running
+(defn grab-call-inputs
+  "Returns the first `n` inputs to the `func-var`, from Running
    the cli args.
 
-  Does this by wrapping the `run-push` function to save each call, and
+  Does this by wrapping `func-var` function to save each call, and
   raising an exception after we have enough calls done, which is then caught."
-  [n cli-args]
-  (let [calls-atom (atom (list))
-        eval-push-default eval-push ;; https://www.reddit.com/r/Clojure/comments/5esre1/how_to_call_the_original_function_in_withredefs/daf401z/
+  [func-var n cli-args]
+  (let [inputs-atom (atom (list))
+        func-original (var-get func-var)
         done-fn #(>= (count %) n)
-        eval-push-log (fn [state & args]
-                        (let [ret (apply eval-push-default state args)
-                              calls (swap! calls-atom conj {:input state :output ret})]
-                          (when (done-fn calls)
+        func-log (fn [& input]
+                        (let [ret (apply func-original input)
+                              inputs (swap! inputs-atom conj input)]
+                          (when (done-fn inputs)
                             (throw (Exception. "")))
                           ret))]
 
-    (with-redefs [eval-push eval-push-log]
-      (try
-        (with-out-str (apply -main cli-args))
-        (catch Exception e
-          (when-not (done-fn @calls-atom)
-            (throw e))))
-      @calls-atom)))
+    (with-redefs-fn {func-var func-log}
+      (fn []
+        (try
+          (with-out-str (apply -main cli-args))
+          (catch Exception e
+            (when-not (done-fn @inputs-atom)
+              (throw e))))))
+    @inputs-atom))
 
-(defmacro defbench-eval-push [number-programs problem]
-  `(defbench ~(symbol (str "eval-push-on-" number-programs "-from-" problem))
-    (println "Grabbing executions...")
-    (let [args# [~problem ":use-single-thread" "true"]
-          executions# (grab-executions ~number-programs args#)
-          inputs# (into [] (map :input executions#))]
+(defmacro defbench-grab-then-benchmark [func-var n-inputs args]
+  `(defbench ~(symbol (str (:name (meta (eval func-var))) "-on-" n-inputs "-from-" (first args)))
+    (println "Grabbing inputs...")
+    (let [total-args# [~@args ":use-single-thread" "true"]
+          inputs# (grab-call-inputs ~func-var ~n-inputs total-args#)
+          func# (var-get ~func-var)]
       (println "Running benchmark...")
       (is (c/quick-bench
             (doseq [input# inputs#]
-              (eval-push input#)))))))
+              (apply func# input#)))))))
 
-(defbench-eval-push 1000 "clojush.problems.software.replace-space-with-newline")
-(defbench-eval-push 1000 "clojush.problems.integer-regression.nth-prime")
-(defbench-eval-push 10000 "clojush.problems.integer-regression.nth-prime")
+(defbench-grab-then-benchmark #'eval-push 1000 ["clojush.problems.software.replace-space-with-newline"])
+(defbench-grab-then-benchmark #'eval-push 1000 ["clojush.problems.integer-regression.nth-prime"])
+(defbench-grab-then-benchmark #'eval-push 10000 ["clojush.problems.integer-regression.nth-prime"])
