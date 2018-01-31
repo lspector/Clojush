73484c2a1d5f062a7c0646ba0ab1a13d854e46db
Lee Spector <lspector@hampshire.edu>
2018-01-31 14:48:36 -0500

    Experiment more with meta-errors

--- src/clojush/evaluate.clj
+++ src/clojush/evaluate.clj
@@ -583,7 +583,7 @@
                            (< e (max p1e p2e)) 1
                            :else 2))))
                 (vec (repeat (count (:errors ind)) 4)))
-              (if (and (:parent1-errors ind)
+              #_(if (and (:parent1-errors ind)
                        (:parent2-errors ind))
                 (vec (for [[e p1e p2e] (mapv #(vector %1 %2 %3)
                                              (:errors ind)
@@ -591,6 +591,20 @@
                                              (:parent2-errors ind))]
                        (cond 
                          (zero? e) 0
+                         (< e (min p1e p2e)) 1
+                         (and (not= p1e p2e) (= e (min p1e p2e))) 2
+                         (< e (max p1e p2e)) 3
+                         (= e (max p1e p2e)) 4
+                         :else 5)))
+                (vec (repeat (count (:errors ind)) 6)))
+              (if (and (:parent1-errors ind)
+                       (:parent2-errors ind))
+                (vec (for [[e p1e p2e] (mapv #(vector %1 %2 %3)
+                                             (:errors ind)
+                                             (:parent1-errors ind)
+                                             (:parent2-errors ind))]
+                       (cond 
+                         (zero? e) 1
                          (< e (min p1e p2e)) 1
                          (and (not= p1e p2e) (= e (min p1e p2e))) 2
                          (< e (max p1e p2e)) 3
