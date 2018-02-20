f84da890a31302b54d69bae895534eb6131bba7d
Lee Spector <lspector@hampshire.edu>
2018-02-20 14:29:20 -0500

    Implement hard-coded stack depth limit in interpreter.clj

--- src/clojush/interpreter.clj
+++ src/clojush/interpreter.clj
@@ -43,61 +43,72 @@
         :else 
         (throw (Exception. (str "Undefined instruction: " (pr-str instruction))))))))
 
+(defn limit-stack-depths
+  [push-state]
+  (into (make-push-state) 
+        (for [[k v] push-state] 
+          [k (if (sequential? v)
+               (apply list (take 10 v))
+               v)])))
+
 (def saved-state-sequence (atom []))
 
 (defn eval-push 
   "Executes the contents of the exec stack, aborting prematurely if execution limits are 
-   exceeded. The resulting push state will map :termination to :normal if termination was 
-   normal, or :abnormal otherwise."
+  exceeded. The resulting push state will map :termination to :normal if termination was 
+  normal, or :abnormal otherwise."
   ([state] (eval-push state false false false))
   ([state print-steps] (eval-push state print-steps false false))
   ([state print-steps trace] (eval-push state print-steps trace false))
   ([state print-steps trace save-state-sequence]
-    ;(when (empty? @global-atom-generators)
-    ;  (println "global-atom-generators is empty. You should do something like: (reset! global-atom-generators '(exec_if boolean_not true false))"))
-    (loop [iteration 1
-           s state
-           time-limit (if (zero? @global-evalpush-time-limit)
-                        0
-                        (+' @global-evalpush-time-limit (System/nanoTime)))]
-      (if (or (> iteration @global-evalpush-limit)
-              (and (empty? (:exec s)) (empty? (:environment s)))
-              (and (not (zero? time-limit))
-                   (> (System/nanoTime) time-limit)))
-        (assoc s :termination (if (and (empty? (:exec s)) (empty? (:environment s)))
-                                :normal
-                                :abnormal))
-        (if (empty? (:exec s))
-          (let [s (end-environment s)]
-            (when print-steps
-              (printf "\nState after %s steps (last step: %s):\n" 
-                      iteration "end_environment_from_empty_exec")
-              (state-pretty-print s))
-            (when save-state-sequence
-              (swap! saved-state-sequence #(conj % s)))
-            (recur (inc iteration) s time-limit))
-          (let [exec-top (top-item :exec s)
-                s (pop-item :exec s)]
-            (let [s (if (seq? exec-top)
-                      (assoc s :exec (concat exec-top (:exec s)))
-                      (let [execution-result (execute-instruction exec-top s)]
-                        (cond
-                          (= trace false) execution-result
-                          (= trace true) (assoc execution-result
-                                                :trace
-                                                (cons exec-top (let [t (:trace s)] (if (seq? t) t ()))))
-                          (= trace :changes) (if (= execution-result s)
-                                               execution-result
-                                               (assoc execution-result
-                                                      :trace
-                                                      (cons exec-top (let [t (:trace s)] (if (seq? t) t ()))))))))]
-              (when print-steps
-                (printf "\nState after %s steps (last step: %s):\n"
-                        iteration (if (seq? exec-top) "(...)" exec-top))
-                (state-pretty-print s))
-              (when save-state-sequence
-                (swap! saved-state-sequence #(conj % s)))
-              (recur (inc iteration) s time-limit))))))))
+   ;(when (empty? @global-atom-generators)
+   ;  (println "global-atom-generators is empty. You should do something like: 
+   ;(reset! global-atom-generators '(exec_if boolean_not true false))"))
+   (loop [iteration 1
+          s (limit-stack-depths state)
+          time-limit (if (zero? @global-evalpush-time-limit)
+                       0
+                       (+' @global-evalpush-time-limit (System/nanoTime)))]
+     (if (or (> iteration @global-evalpush-limit)
+             (and (empty? (:exec s)) (empty? (:environment s)))
+             (and (not (zero? time-limit))
+                  (> (System/nanoTime) time-limit)))
+       (assoc s :termination (if (and (empty? (:exec s)) (empty? (:environment s)))
+                               :normal
+                               :abnormal))
+       (if (empty? (:exec s))
+         (let [s (end-environment s)]
+           (when print-steps
+             (printf "\nState after %s steps (last step: %s):\n" 
+                     iteration "end_environment_from_empty_exec")
+             (state-pretty-print s))
+           (when save-state-sequence
+             (swap! saved-state-sequence #(conj % s)))
+           (recur (inc iteration) s time-limit))
+         (let [exec-top (top-item :exec s)
+               s (pop-item :exec s)]
+           (let [s (limit-stack-depths
+                     (if (seq? exec-top)
+                       (assoc s :exec (concat exec-top (:exec s)))
+                       (let [execution-result (execute-instruction exec-top s)]
+                         (cond
+                           (= trace false) execution-result
+                           (= trace true) (assoc execution-result
+                                            :trace
+                                            (cons exec-top (let [t (:trace s)] (if (seq? t) t ()))))
+                           (= trace :changes) (if (= execution-result s)
+                                                execution-result
+                                                (assoc execution-result
+                                                  :trace
+                                                  (cons exec-top 
+                                                        (let [t (:trace s)] (if (seq? t) t ())))))))))]
+             (when print-steps
+               (printf "\nState after %s steps (last step: %s):\n"
+                       iteration (if (seq? exec-top) "(...)" exec-top))
+               (state-pretty-print s))
+             (when save-state-sequence
+               (swap! saved-state-sequence #(conj % s)))
+             (recur (inc iteration) s time-limit))))))))
 
 (defn run-push 
   "The top level of the push interpreter; calls eval-push between appropriate code/exec 
@@ -121,5 +132,4 @@
           (if @global-top-level-pop-code
             (pop-item :code s)
             s))))))
-
 
