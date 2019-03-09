b8c4e4b9c1e1c1a027229bf4d3e44a565ecacd5d
Lee Spector <lspector@hampshire.edu>
2019-03-09 14:21:07 -0500

    Implement elite-diversity-tournament-lexicase selection but still accessed via elite-count-tournament

--- src/clojush/pushgp/selection/elite_count_tournament_lexicase.clj
+++ src/clojush/pushgp/selection/elite_count_tournament_lexicase.clj
@@ -1,13 +1,13 @@
 (ns clojush.pushgp.selection.elite-count-tournament-lexicase
   (:use [clojush random]))
 
-(defn elite-group-count
-  "Retuns the number of individuals in pop with the best error for the given case."
+(defn elite-diversity
+  "Retuns the number of error vectors in pop with the best error for the given case."
   [pop c]
   (let [min-err-for-case (apply min (map #(nth % c)
                                          (map :errors pop)))]
-    (count (filter #(= (nth (:errors %) c) min-err-for-case)
-                   pop))))
+    (count (distinct (map :errors (filter #(= (nth (:errors %) c) min-err-for-case)
+                                          pop))))))
 
 (defn elite-count-tournament-lexicase-selection
   "Returns an individual that does the best on the fitness cases when considered one at a
@@ -21,7 +21,7 @@
       (lrand-nth survivors)
       (let [case-to-use (let [tournament-set (repeatedly (:elite-count-tournament-size argmap)
                                                          #(lrand-nth cases))]
-                          (apply min-key (partial elite-group-count survivors) tournament-set))
+                          (apply max-key (partial elite-diversity survivors) tournament-set))
             min-err-for-case (apply min (map #(nth % case-to-use)
                                              (map :errors survivors)))]
         (recur (filter #(= (nth (:errors %) case-to-use) min-err-for-case)
