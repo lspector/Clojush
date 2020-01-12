6a14c03c3482a0eb78da661bd7ec9a84c155d8b9
Lee Spector <lspector@hampshire.edu>
2020-01-11 22:20:54 -0500

    Implement and switch to ALT 3 option for :bsw meta-error

--- src/clojush/meta_errors.clj
+++ src/clojush/meta_errors.clj
@@ -387,19 +387,19 @@
                  ;__________________better_0______2______4
                  ;$\delta(e)_{t-1}$_same___1______8______6
                  ;__________________worse__3______7______5
-                 (case delta-e_t
-                   :better (case delta-e_t-1
-                             :better 0
-                             :same 1
-                             :worse 3)
-                   :same (case delta-e_t-1
-                           :better 2
-                           :same 8
-                           :worse 7)
-                   :worse (case delta-e_t-1
-                            :better 4
-                            :same 6
-                            :worse 5))
+                 ;(case delta-e_t
+                 ;  :better (case delta-e_t-1
+                 ;            :better 0
+                 ;            :same 1
+                 ;            :worse 3)
+                 ;  :same (case delta-e_t-1
+                 ;          :better 2
+                 ;          :same 8
+                 ;          :worse 7)
+                 ;  :worse (case delta-e_t-1
+                 ;           :better 4
+                 ;           :same 6
+                 ;           :worse 5))
                  ;ALT 1_______________________$\delta(e)_t$
                  ;_________________________better_same___worse
                  ;__________________better_0______5______2
@@ -436,6 +436,24 @@
                  ;           :better 5
                  ;           :same 7
                  ;           :worse 8))
+                 ;ALT 3_______________________$\delta(e)_t$
+                 ;_________________________better_same___worse
+                 ;__________________better_0______2______4
+                 ;$\delta(e)_{t-1}$_same___1______8______5
+                 ;__________________worse__3______6______7
+                 (case delta-e_t
+                   :better (case delta-e_t-1
+                             :better 0
+                             :same 1
+                             :worse 3)
+                   :same (case delta-e_t-1
+                           :better 2
+                           :same 8
+                           :worse 6)
+                   :worse (case delta-e_t-1
+                            :better 4
+                            :same 5
+                            :worse 7))
                  )))))))
 
 (defn case-unsolved-non-improvement-meta-error              ;; requires neutral-lexicase
