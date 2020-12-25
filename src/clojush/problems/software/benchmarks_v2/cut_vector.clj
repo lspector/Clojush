2a28d43e5b682cf1c5d85005fb3800a704fba533
Tom Helmuth <thelmuth@hamilton.edu>
2020-12-24 22:42:06 -0500

    Updated atom generators for Cut Vector. Not done with sanity check; need to update data domains.

--- src/clojush/problems/software/benchmarks_v2/cut_vector.clj
+++ src/clojush/problems/software/benchmarks_v2/cut_vector.clj
@@ -35,17 +35,15 @@
 
 ; Atom generators
 (def cut-vector-atom-generators
-  (concat (list
-            ;;; end constants
-            (fn [] (cut-vector-input (inc (lrand-int 20)))) ;Vector ERC
-            ;;; end ERCs
-            (tag-instruction-erc [:vector_integer :integer :boolean :exec] 1000)
-            (tagged-instruction-erc 1000)
-            ;;; end tag ERCs
-            'in1
-            ;;; end input instructions
-            )
-          (registered-for-stacks [:vector_integer :integer :boolean :exec])))
+  (make-proportional-atom-generators
+   (concat
+    (registered-for-stacks [:vector_integer :integer :boolean :exec])
+    (list (tag-instruction-erc [:vector_integer :integer :boolean :exec] 1000) ; tags
+          (tagged-instruction-erc 1000)))
+   (list 'in1) ; inputs
+   (list 0 []) ; constants
+   {:proportion-inputs 0.15
+    :proportion-constants 0.05}))
 
 ;; A list of data domains for the problem. Each domain is a vector containing
 ;; a "set" of inputs and two integers representing how many cases from the set
